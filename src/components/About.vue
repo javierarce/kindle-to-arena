d44f7dca02bc9ab7acea952acbadf26c80954d61
Javier Arce <javierarce@users.noreply.github.com>
2020-08-30 19:54:53 +0200

    More info

--- src/components/About.vue
+++ src/components/About.vue
@@ -10,34 +10,39 @@
           </div>
 
           <div class="About__question">
-            <strong class="About__title">How does it work?</strong>
-            <p>Connect your Kindle to your computer, find the file <span class="code">My Clippings.txt</span>, and drop it into this website. You'll see all your books and documents and will be able to send your highlights to any of your Are.na channels.</p>
+            <strong class="About__title">How does this website work?</strong>
+            <p>Connect your Kindle to your computer, find the file <span class="code">My Clippings.txt</span>, and drop it into this very website. You'll see all your books and documents and will be able to send highlights to any of your Are.na channels.</p>
           </div>
 
           <div class="About__question">
-            <strong class="About__title">Do you store that file somewhere?</strong>
-            <p>Nope.</p>
+            <strong class="About__title">Do you store that clippings file somewhere?</strong>
+            <p>Nope! The file is sent to the server to extract the highlights and notes, but they are not stored anywhere.</p>
           </div>
 
           <div class="About__question">
           <strong class="About__title">But if I reload the page the highlights are there!</strong>
-          <p>They are stored in the Local Storage of your browser.</p>
+          <p>Yes, they are stored in the Local Storage of your browser.</p>
+          </div>
+
+          <div class="About__question">
+          <strong class="About__title">I found a typo/want to edit the highlight before sending it</strong>
+          <p>No problem, you can click on any text and change it. You can even use Markdown to format the highlight to your liking.</p>
           </div>
 
           <div class="About__question">
           <strong class="About__title">What's with 'We couldn't import X highlights' message?</strong>
-          <p>I'm afraid that some original Kindle books have stricts limits on how many highlights you can save :(</p>
+          <p>I'm afraid that some original Kindle books have strict limits on how many highlights you can save :(</p>
           </div>
 
           <div class="About__question">
-          <strong class="About__title">I found a bug and/or have a suggestion</strong>
-          <p><a href="mailto://javier@hey.com">Send me a message</a> or better yet create an issue <a href="https://github.com/javierarce/kindle-to-arena">in this repo</a> Thanks!</p>
+          <strong class="About__title">I found a bug/have a suggestion</strong>
+          <p>Sorry slash thanks! <a href="mailto://javier@hey.com">Send me a message</a> or better yet create an issue <a href="https://github.com/javierarce/kindle-to-arena">in this GitHub repo</a>.</p>
           </div>
         </div>
 
         <div class="About__footer">
           <span>
-            Made by <a href="https://twitter.com/javier">Javier Arce</a>
+            Made by <a href="https://twitter.com/javier" target="_blank">Javier Arce</a></a>
           </span>
         </div>
       </div>
