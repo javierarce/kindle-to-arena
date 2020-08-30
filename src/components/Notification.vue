d2f8242a843292855e86385c76ff5303de24ab0d
Javier Arce <javierarce@users.noreply.github.com>
2020-08-30 16:23:22 +0200

    Don't hide the notifications if the user is hovering the div

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Notification">
     <transition :name="transitionName">
-    <div class="Notification__message" v-html="message" v-if="show"></div>
+      <div class="Notification__message" v-html="message" v-if="show" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave"></div>
     </transition>
   </div>
 </template>
@@ -10,7 +10,7 @@
 import mixins from '../mixins'
 import config from '../../config'
 
-const TIME_OUT = 5000
+const TIME_OUT = 7000
 const TRANSITION_NAME = 'notification'
 
 export default {
@@ -34,6 +34,19 @@ export default {
       window.bus.$off(config.ACTIONS.SHOW_MESSAGE)
       window.bus.$on(config.ACTIONS.SHOW_MESSAGE, this.showMessage)
     },
+    onMouseEnter () {
+      if (this.timeoutID) {
+        clearTimeout(this.timeoutID)
+      }
+    },
+    onMouseLeave () {
+      this.hide()
+    },
+    hide () {
+      this.timeoutID = setTimeout(() => {
+        this.show = false
+      }, TIME_OUT)
+    },
     publishMessage (message) {
       this.message = message
       this.show = true
@@ -42,9 +55,7 @@ export default {
         clearTimeout(this.timeoutID)
       }
 
-      this.timeoutID = setTimeout(() => {
-        this.show = false
-      }, TIME_OUT)
+      this.hide()
     },
     showMessage (message) {
       if (this.message && this.message !== message) {
