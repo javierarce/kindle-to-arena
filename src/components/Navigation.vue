e2e5d5461fdb17e775d489c16a74304a67412750
Javier Arce <javierarce@users.noreply.github.com>
2020-09-01 09:21:28 +0200

    Adds title

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="Navigation">
+    <h1 class="Navigation__title">Kindle to Are.na</h1>
     <div class="Navigation__actions">
       <button class="Button" @click="onClickAbout">About</button>
       <a :href="URL" class="Button">
